af5100d5bd6736b1f5c6dd9cf82d6c4cc9742dbe
Romain GILOT <romain.gilot85@gmail.com>
2024-05-04 17:42:30 +0200

    Modificatio responsive projet

--- src/components/Projets/Projets.jsx
+++ src/components/Projets/Projets.jsx
@@ -9,10 +9,14 @@ import mns from "../../images/logo-mns.svg";
 function Projets() {
   return (
     <>
-      <div className="bg-gray-50 p-10 " id="mes-projets">
+      <div className="bg-gray-50 pt-10 " id="mes-projets">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-base font-semibold bg-[#EAF9FC] rounded p-1 text-[#5FACD3] inline-block">Mes projets</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-10">J'ai réalisé quelques projets, regardez les !</p>
+          <h2 className="text-base font-semibold bg-[#EAF9FC] rounded p-1 text-[#5FACD3] inline-block">
+            Mes projets
+          </h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-10">
+            J'ai réalisé quelques projets, regardez les !
+          </p>
           <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
             <Box
               title="Portfolio"
@@ -20,21 +24,40 @@ function Projets() {
               message="Ce projet est le site sur lequel vous vous trouvez actuellement ! Bonne visite :)"
               projectName="portfolio"
             />
-            <Box title="GDEPT" img={sdis57} message="Ce projet a été réalisé dans le cadre de mon alternance au SDIS57." projectName="gdept" />
-            <Box title="Access" img={badge} message="Ce projet a été réalisé dans le cadre de mon bachelor Développeur Full-Stack." projectName="access" />
+            <Box
+              title="GDEPT"
+              img={sdis57}
+              message="Ce projet a été réalisé dans le cadre de mon alternance au SDIS57."
+              projectName="gdept"
+            />
+            <Box
+              title="Access"
+              img={badge}
+              message="Ce projet a été réalisé dans le cadre de mon bachelor Développeur Full-Stack."
+              projectName="access"
+            />
           </div>
         </div>
       </div>
       <div className="py-12 bg-gray-50" id="parcours">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-base font-semibold bg-[#EAF9FC] rounded p-1 text-[#5FACD3] inline-block">Mon parcours</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-10">Quel est mon parcours ?</p>
+          <h2 className="text-base font-semibold bg-[#EAF9FC] rounded p-1 text-[#5FACD3] inline-block">
+            Mon parcours
+          </h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-10">
+            Quel est mon parcours ?
+          </p>
 
           <dl className="grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-3">
-            <a href="#" className="flex items-center ms:mx-auto hover:scale-105 delay-75 transform transition-transform duration-300 cursor-pointer">
+            <a
+              href="#"
+              className="flex items-center ms:mx-auto hover:scale-105 delay-75 transform transition-transform duration-300 cursor-pointer"
+            >
               <img src={juliedaubie} className="w-28 h-28" />
               <div className="ms-10 flex max-w-xs flex-col gap-y-4">
-                <span className="text-2xl  tracking-tight text-gray-900">Baccalauréat Général</span>
+                <span className="text-2xl  tracking-tight text-gray-900">
+                  Baccalauréat Général
+                </span>
                 <dd className="order-first text-gray-600">2018-2021</dd>
               </div>
             </a>
@@ -44,7 +67,9 @@ function Projets() {
             >
               <img src={robertschuman} className="w-28 h-28" />
               <div className="ms-10 flex max-w-xs flex-col gap-y-4">
-                <span className="text-2xl  tracking-tight text-gray-900">BTS SIO SLAM</span>
+                <span className="text-2xl  tracking-tight text-gray-900">
+                  BTS SIO SLAM
+                </span>
                 <dd className="order-first text-gray-600">2021 - 2023</dd>
               </div>
             </a>
@@ -54,7 +79,9 @@ function Projets() {
             >
               <img src={mns} className="w-28 h-28" />
               <div className="ms-10 flex max-w-xs flex-col gap-y-4">
-                <span className="text-2xl  tracking-tight text-gray-900">Bachelor Développeur Full-Stack</span>
+                <span className="text-2xl  tracking-tight text-gray-900">
+                  Bachelor Développeur Full-Stack
+                </span>
                 <dd className="text-gray-600 order-first">2023 - 2024</dd>
               </div>
             </a>
